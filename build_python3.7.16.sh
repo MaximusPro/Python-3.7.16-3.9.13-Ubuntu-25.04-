8f44f25d12e8b23c4e47b37ce8a4f902f985841d
MaximusPro <maximusvj@gmail.com>
2026-03-14 19:15:02 -0400

    Add script to build and install Python 3.7.16
    This script installs Python 3.7.16 with OpenSSL support and custom installation directory.

---
+++ build_python3.7.16.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt install libssl-dev libffi-dev zlib1g-dev build-essential
+sudo wget https://www.python.org/ftp/python/3.7.16/Python-3.7.16.tgz
+sudo tar -xzf Python-3.7.16.tgz
+cd Python-3.7.16
+#bin/bash
+OPENSSL_DIR="/usr/local/openssl-3.0.18"
+CUSTOM_PYTHON_DIR="/usr/local/python-3.7.16-custom"
+export CPPFLAGS="-I${OPENSSL_DIR}/include"
+export LDFLAGS="-L${OPENSSL_DIR}/lib -Wl,-rpath,${OPENSSL_DIR}/lib"
+export LD_LIBRARY_PATH="${OPENSSL_DIR}/lib:${LD_LIBRARY_PATH}"
+export PKG_CONFIG_PATH="${OPENSSL_DIR}/lib/pkgconfig:${PKG_CONFIG_PATH}"
+echo $LDFLAGS
+
+sudo ./configure --prefix=${CUSTOM_PYTHON_DIR} --enable-optimizations --with-ensurepip=install
+
+make -j$(nproc)
+sudo make altinstall
+sudo rm Python-3.7.16.tgz
+sudo rm -rf Python-3.7.16
+echo "Path to Python: ${CUSTOM_PYTHON_DIR}"
+${CUSTOM_PYTHON_DIR}/bin/python3 --version
+
